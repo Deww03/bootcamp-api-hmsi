4ef9fbec6a27f5c06fd8e38288c2ecb408a478fd
Deww03 <kurniawanadams72@gmail.com>
2023-07-30 11:57:24 +0700

    membuat API

---
+++ modules/customers/customerUsecase/customerUsecase.go
@@ -0,0 +1,33 @@
+package customerUsecase
+
+import (
+	"github.com/Deww03/bootcamp-api-hmsi/models"
+	"github.com/Deww03/bootcamp-api-hmsi/modules/customers"
+)
+
+type customerRepository struct {
+	Repo customers.CustomerRepository
+}
+
+func NewCustomerUsecase(Repo customers.CustomerRepository) customers.CustomerUsecase {
+	return &customerRepository{Repo}
+}
+
+func (r *customerRepository) FindAll() (*[]models.Customers, error) {
+	result, err := r.Repo.GetAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+// func (r *customerRepository) Insert(c *models.Customers)error {
+// 	err := r.Repo.Create(c)
+
+// 	if err!= nil {
+//         return err
+//     }
+// 	return nil
+// }
