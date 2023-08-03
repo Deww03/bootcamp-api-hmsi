c500a86cbdb655deb9fcc5aaeac2267150071756
Adam Kurniawan <kurniawanadams72@gmail.com>
2023-08-03 12:15:50 +0700

    Update & Delete API

--- modules/customers/customers.go
+++ modules/customers/customers.go
@@ -6,10 +6,14 @@ type (
 	CustomerRepository interface {
 		GetAll() (*[]models.Customers, error)
 		Create(c *models.RequestInsertCustomer) error
+		Update(c *models.RequestUpdateCustomer) error
+		Delete(id uint64) error
 	}
 
 	CustomerUsecase interface {
 		FindAll() (*[]models.Customers, error)
 		Insert(c *models.RequestInsertCustomer) error
+		Update(c *models.RequestUpdateCustomer) error
+		Delete(id uint64) error
 	}
 )
